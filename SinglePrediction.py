9ca52d544a367b27942319400666dc3091c2af42
widyamustika <widya.mustikasarii@gmail.com>
2025-06-09 11:20:04 +0700

    model error handling

--- SinglePrediction.py
+++ SinglePrediction.py
@@ -37,10 +37,10 @@ def show_single():
             st.subheader(f"{model_name} Prediction: {pred[0]} → {label}")
 
         if use_knn:
-            show_prediction("K-Nearest Neighbors", "modelJB_KNN.joblib")
+            show_prediction("K-Nearest Neighbors", "modelJb_KNN.joblib")
         if use_svm:
-            show_prediction("Support Vector Machine", "modelJB_SVM.joblib")
+            show_prediction("Support Vector Machine", "modelJb_SVM.joblib")
         if use_nn:
-            show_prediction("Neural Network", "modelJB_NN.joblib")
+            show_prediction("Neural Network", "modelJb_NN.joblib")
         if use_dt:
-            show_prediction("Decision Tree", "modelJB_DecisionTree.joblib")
+            show_prediction("Decision Tree", "modelJb_DecisionTree.joblib")
